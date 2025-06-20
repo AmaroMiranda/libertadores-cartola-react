f3e136c5951da83d988b329b52369757fefe90f2
AmaroMiranda <amaro.miranda2025@outlook.com>
2025-06-20 07:47:31 -0300

    added dotenv

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Buffer } from "buffer";
-import { AuthProvider } from "./context/AuthContext"; 
+import { AuthProvider } from "./context/AuthContext";
 
 window.Buffer = Buffer;
 
@@ -11,7 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    {/* ENVOLVER O APP COM O PROVIDER */}
     <AuthProvider>
       <App />
     </AuthProvider>
